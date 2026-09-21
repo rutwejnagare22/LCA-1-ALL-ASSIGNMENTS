2acbe1cbefd36c72e63e98e9943d4828929c666e
Rutwej Nagare <rutwejnagare22@gmail.com>
2026-09-21 19:54:19 +0530

    Add files via upload

---
+++ PROGRAM 3.py
@@ -0,0 +1,8 @@
+Write a python program to accept two numbers from the user and print their sum.
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+sum = num1 + num2
+
+print("The sum is:", sum)
